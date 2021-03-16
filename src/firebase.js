c7ca0a69418e3a35ab30ad5bd8456e46e68727c8
AlbertCos <cosmenalberto@gmail.com>
2021-03-16 21:22:19 +0100

    Start

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB7Tk9YMAP_0sii6nfZ9POOHVh-ABMF8GI",
+//     authDomain: "netflixclone-d4da1.firebaseapp.com",
+//     projectId: "netflixclone-d4da1",
+//     storageBucket: "netflixclone-d4da1.appspot.com",
+//     messagingSenderId: "933805473948",
+//     appId: "1:933805473948:web:956b511ea7733c5d396d59"
+//   };
