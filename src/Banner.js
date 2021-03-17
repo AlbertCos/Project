cd9152d3d15e8439997ebdefc086762d8a19cbcb
AlbertCos <cosmenalberto@gmail.com>
2021-03-17 18:52:09 +0100

    developing Banner

---
+++ src/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import'./Banner.css';
+
+function Banner() {
+    return (
+        <header className= 'banner' style={{
+            backgroundSize:'cover',
+            backgroundImage:`url("https://i.imgur.com/pChHk8F.jpg')`,
+            backgroundPosition:'center center',
+        }}>
+            <div className="banner__contents">
+                <h1 className="banner__title">Movie Name</h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Banner
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import './HomeScreen.css';
 import Nav from './Nav';
+import Banner from './Banner';
 
 function HomeScreen() {
     return (
         <div className='homeScreen'>
-            {/*Nav*/}
              <Nav />
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
-             <h1>TEST</h1>
 
-            {/* Banner */}
-            {/* Rows */}
+            <Banner/>
+            
+            
         </div>
     )
 }
